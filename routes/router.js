42d9faa1c1e200f9462c9d2c5be4a15cee752f62
Evander Oktapian <evandertambunan@gmail.com>
2023-10-19 17:41:55 +0700

    final bet ini

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 const express = require ('express')
 const router = express.Router()
-const {verifyTokens, extractToken, verifyAdminTokens} = require("../middleware/jwt")
+const {verifyAdminTokens} = require("../middleware/jwt")
 
 
 // admin
